1963ae8d1abac81e6151d202a003b020437f6b64
Petru Orbu <whhroo@Petrus-MBP.Dlink>
2023-11-05 01:12:01 +0200

    Viewport Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Onest as font } from 'next/font/google';
 import '../styles/global.css';
 import { cn } from '@/lib/utils';
@@ -13,9 +13,14 @@ const onest = font({
 
 export const metadata: Metadata = {
     title: "Mealtime - Mai mult decât un simplu ceai - It's Over 9000!",
-    viewport: 'width=device-width, initial-scale=1, maximum-scale=1', // <-- now here
     description:
         'Proprietățile înalte gustative ale produselor noastre sunt obținute datorită utilizării exclusiv a unor materii prime naturale, precum și a diferitelor metode și tehnici culinare, care sunt perfect stăpânite de bucătarii noștri. Noi din principiu nu folosim coloranți, potențiatori de aromă și conservanți, iar termenul îndelungat de păstrare îl obținem datorită utilizării în producție a înghețării cu șoc. Tuturor le sunt cunoscute proprietățile benefice ale ceaiurilor și limonadelor naturale de casă din pomușoare, al căror gust atrage atât adulții cât și copiii, dar pregătirea unei astfel de băuturi durează foarte mult și necesită abilități culinare considerabile. Cu ajutorul concentratelor ceaiului de casă sau a limonadei, pregătirea băuturilor a devenit mai simplă, rapidă și mai comodă.',
+};
+
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
