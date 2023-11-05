e7e32b9f27249d7e8dec586d3fb9f3e51f0d71cd
Petru Orbu <whhroo@Petrus-MBP.Dlink>
2023-11-05 15:02:08 +0200

    admin-charts

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,9 @@
+import RecentSales from '@/components/blocks/RecentSales';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Building2, CreditCard, DollarSign, Package } from 'lucide-react';
 import { Metadata } from 'next';
+import { BarChart, LineChart } from '@tremor/react';
+import { formatPrice } from '@/lib/utils';
 
 export const metadata: Metadata = {
     title: 'Dashboard',
@@ -6,9 +11,160 @@ export const metadata: Metadata = {
 };
 
 export default function Dashboard() {
+    const chartdata = [
+        {
+            name: 'Rompetrrol',
+            'Suma comenzilor (MDL)': 126261,
+        },
+        {
+            name: 'Vento',
+            'Suma comenzilor (MDL)': 29632,
+        },
+        {
+            name: 'Petrom',
+            'Suma comenzilor (MDL)': 78251,
+        },
+        {
+            name: 'Bemol',
+            'Suma comenzilor (MDL)': 51521,
+        },
+        {
+            name: 'Avante',
+            'Suma comenzilor (MDL)': 15237,
+        },
+        {
+            name: 'Lukoil',
+            'Suma comenzilor (MDL)': 20236,
+        },
+        {
+            name: 'Now',
+            'Suma comenzilor (MDL)': 12032,
+        },
+    ];
+
+    const ordersStatistics = [
+        {
+            date: 'Ian 23',
+            'Nr. de comenzi': 12,
+            'Venit din comenzi': 12592,
+        },
+        { date: 'Feb 23', 'Nr. de comenzi': 18, 'Venit din comenzi': 28920 },
+        { date: 'Mar 23', 'Nr. de comenzi': 22, 'Venit din comenzi': 14830 },
+        { date: 'Apr 23', 'Nr. de comenzi': 17, 'Venit din comenzi': 16200 },
+        { date: 'Mai 23', 'Nr. de comenzi': 20, 'Venit din comenzi': 27340 },
+        { date: 'Iun 23', 'Nr. de comenzi': 15, 'Venit din comenzi': 13050 },
+        { date: 'Iul 23', 'Nr. de comenzi': 48, 'Venit din comenzi': 52760 },
+        { date: 'Aug 23', 'Nr. de comenzi': 19, 'Venit din comenzi': 15080 },
+        { date: 'Sep 23', 'Nr. de comenzi': 21, 'Venit din comenzi': 19590 },
+        { date: 'Oct 23', 'Nr. de comenzi': 11, 'Venit din comenzi': 12550 },
+        { date: 'Noi 23', 'Nr. de comenzi': 62, 'Venit din comenzi': 65927 },
+        { date: 'Dec 23', 'Nr. de comenzi': null, 'Venit din comenzi': null },
+    ];
+
     return (
         <>
-            <h1 className="py-20 text-center">Dashboard în dezvoltare</h1>
+            {/* <h1 className="py-20 text-center">Dashboard în dezvoltare</h1> */}
+
+            <div className="dashboard py-5">
+                <div className="container">
+                    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                        <Card>
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">Venit Total</CardTitle>
+                                <DollarSign className="h-4 w-4 text-muted-foreground"></DollarSign>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="md:text-2xl text-xl font-bold">45.231,89 MDL</div>
+                                <p className="text-xs text-muted-foreground">
+                                    <span className="text-green-600">+20.1%</span> de luna trecută
+                                </p>
+                            </CardContent>
+                        </Card>
+                        <Card>
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">Comenzi</CardTitle>
+                                <CreditCard className="h-4 w-4 text-muted-foreground"></CreditCard>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="md:text-2xl text-xl font-bold">5.291</div>
+                                <p className="text-xs text-muted-foreground">
+                                    <span className="text-green-600">+69.1%</span> de luna trecută
+                                </p>
+                            </CardContent>
+                        </Card>
+                        <Card>
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">Clienți</CardTitle>
+                                <Building2 className="h-4 w-4 text-muted-foreground"></Building2>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="md:text-2xl text-xl font-bold">82</div>
+                                <p className="text-xs text-muted-foreground">
+                                    <span className="text-green-600">+30</span> de anul trecut
+                                </p>
+                            </CardContent>
+                        </Card>
+                        <Card>
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">Produse</CardTitle>
+                                <Package className="h-4 w-4 text-muted-foreground"></Package>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="md:text-2xl text-xl font-bold">11</div>
+                                <p className="text-xs text-muted-foreground">Produse în vânzare</p>
+                            </CardContent>
+                        </Card>
+                        <Card className="col-span-2">
+                            <CardHeader>
+                                <CardTitle>Recent Sales</CardTitle>
+                                <CardDescription>You made 265 sales this month.</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <RecentSales />
+                            </CardContent>
+                        </Card>
+
+                        <Card className="col-span-2">
+                            <CardHeader>
+                                <CardTitle>Suma comenzilor pe companie</CardTitle>
+                                <CardDescription>
+                                    Suma totala a comenzilor create pe platforma Mealtime
+                                </CardDescription>
+                            </CardHeader>
+
+                            <CardContent>
+                                <BarChart
+                                    data={chartdata}
+                                    index="name"
+                                    categories={['Suma comenzilor (MDL)']}
+                                    showAnimation
+                                    yAxisWidth={48}
+                                />
+                            </CardContent>
+                        </Card>
+                        <Card className="col-span-4">
+                            <CardHeader>
+                                <CardTitle>Statistica vânzărilor pe lună</CardTitle>
+                                <CardDescription>
+                                    Suma si numarul total al vânzărilor efectuate prin platforma
+                                    MealTime
+                                </CardDescription>
+                            </CardHeader>
+
+                            <CardContent>
+                                <LineChart
+                                    data={ordersStatistics}
+                                    index="date"
+                                    showAnimation
+                                    connectNulls={true}
+                                    categories={['Nr. de comenzi', 'Venit din comenzi']}
+                                    yAxisWidth={50}
+                                />
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
--- components/blocks/Header.tsx
+++ components/blocks/Header.tsx
@@ -12,28 +12,50 @@ function Header() {
     const currentRoute = usePathname();
     const dispatch = useDispatch();
     const productsCount = useSelector((state: any) => state.cart.products.length);
+    const isAdmin = false;
     return (
-        <header className="py-4 border-b sticky top-0 w-full bg-white z-10">
+        <header className="py-3 border-b sticky top-0 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-10">
             <div className="container flex gap-5 items-center">
                 <Link href="/dashboard">
-                    <img src="/images/logo.png" alt="MealTime" className="w-16" />
+                    <img src="/images/logo.png" alt="MealTime" className="w-14 h-14" />
                 </Link>
 
-                <nav className="ml-auto flex gap-4">
-                    <Link href="/dashboard" className={currentRoute === '/dashboard' ? active : ''}>
-                        Dashboard
-                    </Link>
-                    <Link
-                        href="/dashboard/shop"
-                        className={currentRoute === '/dashboard/shop' ? active : ''}>
-                        Magazin
-                    </Link>
-                </nav>
+                {!isAdmin && (
+                    <nav className="ml-auto flex gap-4">
+                        <Link
+                            href="/dashboard"
+                            className={currentRoute === '/dashboard' ? active : ''}>
+                            Dashboard
+                        </Link>
+                        <Link
+                            href="/dashboard/shop"
+                            className={currentRoute === '/dashboard/shop' ? active : ''}>
+                            Magazin
+                        </Link>
+                    </nav>
+                )}
 
-                <button onClick={() => dispatch(toggleCart())} className="relative">
-                    <Badge className="absolute -top-3 -right-3">{productsCount}</Badge>
-                    <ShoppingCartIcon></ShoppingCartIcon>
-                </button>
+                {isAdmin && (
+                    <nav className="ml-auto flex gap-4">
+                        <Link
+                            href="/dashboard/users"
+                            className={currentRoute === '/dashboard/clients' ? active : ''}>
+                            Clienti
+                        </Link>
+                        <Link
+                            href="/dashboard/orders"
+                            className={currentRoute === '/dashboard/orders' ? active : ''}>
+                            Comenzi
+                        </Link>
+                    </nav>
+                )}
+
+                {!isAdmin && (
+                    <button onClick={() => dispatch(toggleCart())} className="relative">
+                        <Badge className="absolute -top-3 -right-3">{productsCount}</Badge>
+                        <ShoppingCartIcon></ShoppingCartIcon>
+                    </button>
+                )}
 
                 <Avatar className="w-11 h-11 cursor-pointer">
                     <AvatarImage src="https://github.com/shadcn.png" />
